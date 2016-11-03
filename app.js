0d51f757c5cfeab93c16c09f12cf2cd743cfed0e
dio <abrahamdio@gmail.com>
2016-11-02 22:49:34 -0500

    change to ejs

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ var app = express();
 
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'build'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
