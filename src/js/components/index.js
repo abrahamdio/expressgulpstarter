a9d27683365dce9b34d21106625512e961e24ac8
dio <abrahamdio@gmail.com>
2016-11-02 22:40:10 -0500

    test

---
+++ src/js/components/index.js
@@ -0,0 +1,13 @@
+import angular from 'angular';
+
+let componentsModule = angular.module('app.components', []);
+
+// Components (and directives)
+import ListErrors from './list-errors.component';
+componentsModule.component('listErrors', ListErrors);
+
+
+import ShowAuthed from './show-authed.directive';
+componentsModule.directive('showAuthed', ShowAuthed);
+
+export default componentsModule;
